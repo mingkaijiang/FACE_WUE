54a536206fcb4278d6aafe3aad3f17df005ab059
mingkaijiang <shanyujordan512@gmail.com>
2021-03-31 13:30:13 +1100

    added plots

--- run_program_AG_rev.R
+++ run_program_AG_rev.R
@@ -54,12 +54,13 @@ myDF <- myDF[myDF$Treatment %in%c("Ambient CO2", "Elevated CO2"),]
 myDF$Season <- as.factor(myDF$Season)
 
 ## Filter to just have the summer season
-myDF <- filter(myDF, Season == "summer")    ##If I just wanted to look  at summer responses.
+#myDF <- filter(myDF, Season == "summer")    ##If I just wanted to look  at summer responses.
 myDF<- drop.levels(myDF)
 
 ### Bader_ACI_curves_enhancement_ratios dataset has no VPD values, give it a random value
 #myDF$VPD[myDF$Dataset=="Bader_ACI_curves_enhancement_ratios"] <- 1.0
 myDF <- myDF[myDF$Dataset!="Bader_ACI_curves_enhancement_ratios",]
+myDF <- myDF[!is.na(myDF$VPD), ]
 
 ### G1 values
 myDF$fitgroup<-as.factor(myDF$fitgroup)
@@ -76,22 +77,22 @@ myDF$Location[myDF$Location =="EucFACE Richmond"] <- "EucFACE"
 myDF <- myDF[complete.cases(myDF$Cond),]
 
 
-graph_all <- function (myDF) {
-  # checking model fits
-  list <- split(myDF,myDF$Dataset)
-  fit <- lapply(list,fitBB,gsmodel="BBOpti",
-                varnames=list(VPD="VPD",ALEAF="Photo",GS="Cond",Ca="CO2S"))
-  g1pars <- sapply(fit,function(x)x$coef[[2]])
-  g1pars <- stack(g1pars)
-  g1pars
-  do.call(rbind, lapply(fit, coef))
-  with(myDF,plot(Photo/sqrt(VPD)/CO2S,Cond,col=Dataset))#, group=Dataset)) #fitgroup
-  #legend(x = 0.00, y = 2.0, legend = levels(myDF$Dataset),col = c(1:10), pch=1)
-}
-
-pdf(paste0(getwd(), "/output/photo_vs_cond.pdf"))
-graph_all(myDF)
-dev.off()
+#graph_all <- function (myDF) {
+#  # checking model fits
+#  list <- split(myDF,myDF$Dataset)
+#  fit <- lapply(list,fitBB,gsmodel="BBOpti",
+#                varnames=list(VPD="VPD",ALEAF="Photo",GS="Cond",Ca="CO2S"))
+#  g1pars <- sapply(fit,function(x)x$coef[[2]])
+#  g1pars <- stack(g1pars)
+#  g1pars
+#  do.call(rbind, lapply(fit, coef))
+#  with(myDF,plot(Photo/sqrt(VPD)/CO2S,Cond,col=Dataset))#, group=Dataset)) #fitgroup
+#  #legend(x = 0.00, y = 2.0, legend = levels(myDF$Dataset),col = c(1:10), pch=1)
+#}
+#
+#pdf(paste0(getwd(), "/output/photo_vs_cond.pdf"))
+#graph_all(myDF)
+#dev.off()
 
 ### Let's check VPD distribution in different dataset
 p1 <- ggplot(myDF, aes(x=VPD))+
@@ -225,10 +226,13 @@ sDF$CO2_resp <- with(sDF, CO2.mean.eCO2/CO2.mean.aCO2)
 sDF <- subset(sDF, CO2_resp > 1)
 
 
+### calculate response ratios
 sDF$Photo_resp <- with(sDF, log(Photo.mean.eCO2/Photo.mean.aCO2)/log(CO2_resp))
 sDF$Cond_resp <- with(sDF, log(Cond.mean.eCO2/Cond.mean.aCO2)/log(CO2_resp))
 sDF$WUE_resp <- with(sDF, log(WUE.mean.eCO2/WUE.mean.aCO2)/log(CO2_resp))
 
+
+### calculate variance
 sDF$Photo_var <- with(sDF, 
                       (Photo.sd.eCO2 * Photo.sd.eCO2 / (Photo.n.eCO2 * Photo.mean.eCO2 * Photo.mean.eCO2) +
                           Photo.sd.aCO2 * Photo.sd.aCO2 / (Photo.n.aCO2 * Photo.mean.aCO2 * Photo.mean.aCO2))/log(CO2_resp))
@@ -273,12 +277,12 @@ wueDF.gym <- subset(wueDF, Type=="gymnosperm")
 
 
 ### multi-variate linear mixed effect model
-res_WUE1 <- rma.mv(WUE_resp, WUE_var, mods = ~VPDmean, random = ~1|Dataset, data = wueDF.ang)
-res_WUE2 <- rma.mv(WUE_resp, WUE_var, mods = ~VPDmean, random = ~1|Dataset, data = wueDF.gym)
+res_WUE1 <- rma.mv(WUE_resp, WUE_var, mods = ~VPD_group, random = ~1|Dataset, data = wueDF.ang)
+res_WUE2 <- rma.mv(WUE_resp, WUE_var, mods = ~VPD_group, random = ~1|Dataset, data = wueDF.gym)
 
 ### predict effect size, at different VPD values
-predDF1 <- predict(res_WUE1, newmods = c(0.5, 1.0, 1.5), addx=T)
-predDF2 <- predict(res_WUE2, newmods = c(0.5, 1.0, 1.5), addx=T)
+predDF1 <- predict(res_WUE1, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
+predDF2 <- predict(res_WUE2, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
 
 predDF1$lab <- "Angiosperm"
 predDF2$lab <- "Gymnosperm"
@@ -287,12 +291,25 @@ plotDF1 <- rbind(as.data.frame(predDF1), as.data.frame(predDF2))
 
 ### make a plot
 p1 <- ggplot(plotDF1) +
-  geom_col(aes(X.VPDmean, pred, fill=lab, group=lab),
+  geom_col(aes(X.VPD_group, pred, fill=lab, group=lab),
            position=position_dodge(), col="black")+
-  geom_errorbar(aes(x=X.VPDmean, ymin=pred-se, ymax=pred+se, group=lab), 
-                position=position_dodge(), width = 0.4)
+  geom_errorbar(aes(x=X.VPD_group, ymin=pred-se, ymax=pred+se, group=lab), 
+                position=position_dodge(), width = 0.4)+
+  theme_linedraw() +
+  theme(panel.grid.minor=element_blank(),
+        axis.title.x = element_text(size=12), 
+        axis.text.x = element_text(size=12),
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_text(size=12),
+        legend.text=element_text(size=10),
+        legend.title=element_text(size=12),
+        panel.grid.major=element_blank(),
+        legend.position="right",
+        legend.text.align=0)+
+  xlab("VPD (kPa)")+
+  ylab("WUE CO2 response")
 
-plot(p1)
+#plot(p1)
 
 
 
@@ -305,12 +322,12 @@ condDF.gym <- subset(condDF, Type=="gymnosperm")
 
 
 ### multi-variate linear mixed effect model
-res_cond1 <- rma.mv(Cond_resp, Cond_var, mods = ~VPDmean, random = ~1|Dataset, data = condDF.ang)
-res_cond2 <- rma.mv(Cond_resp, Cond_var, mods = ~VPDmean, random = ~1|Dataset, data = condDF.gym)
+res_cond1 <- rma.mv(Cond_resp, Cond_var, mods = ~VPD_group, random = ~1|Dataset, data = condDF.ang)
+res_cond2 <- rma.mv(Cond_resp, Cond_var, mods = ~VPD_group, random = ~1|Dataset, data = condDF.gym)
 
 ### predict effect size, at different VPD values
-predDF1 <- predict(res_cond1, newmods = c(0.5, 1.0, 1.5), addx=T)
-predDF2 <- predict(res_cond2, newmods = c(0.5, 1.0, 1.5), addx=T)
+predDF1 <- predict(res_cond1, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
+predDF2 <- predict(res_cond2, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
 
 predDF1$lab <- "Angiosperm"
 predDF2$lab <- "Gymnosperm"
@@ -319,12 +336,25 @@ plotDF2 <- rbind(as.data.frame(predDF1), as.data.frame(predDF2))
 
 ### make a plot
 p2 <- ggplot(plotDF2) +
-  geom_col(aes(X.VPDmean, pred, fill=lab, group=lab),
+  geom_col(aes(X.VPD_group, pred, fill=lab, group=lab),
            position=position_dodge(), col="black")+
-  geom_errorbar(aes(x=X.VPDmean, ymin=pred-se, ymax=pred+se, group=lab), 
-                position=position_dodge(), width = 0.4)
+  geom_errorbar(aes(x=X.VPD_group, ymin=pred-se, ymax=pred+se, group=lab), 
+                position=position_dodge(), width = 0.4)+
+  theme_linedraw() +
+  theme(panel.grid.minor=element_blank(),
+        axis.title.x = element_text(size=12), 
+        axis.text.x = element_text(size=12),
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_text(size=12),
+        legend.text=element_text(size=10),
+        legend.title=element_text(size=12),
+        panel.grid.major=element_blank(),
+        legend.position="right",
+        legend.text.align=0)+
+  xlab("VPD (kPa)")+
+  ylab("Conductance CO2 response")
 
-plot(p2)
+#plot(p2)
 
 
 
@@ -337,12 +367,12 @@ photoDF.gym <- subset(photoDF, Type=="gymnosperm")
 
 
 ### multi-variate linear mixed effect model
-res_photo1 <- rma.mv(Photo_resp, Photo_var, mods = ~VPDmean, random = ~1|Dataset, data = photoDF.ang)
-res_photo2 <- rma.mv(Photo_resp, Photo_var, mods = ~VPDmean, random = ~1|Dataset, data = photoDF.gym)
+res_photo1 <- rma.mv(Photo_resp, Photo_var, mods = ~VPD_group, random = ~1|Dataset, data = photoDF.ang)
+res_photo2 <- rma.mv(Photo_resp, Photo_var, mods = ~VPD_group, random = ~1|Dataset, data = photoDF.gym)
 
 ### predict effect size, at different VPD values
-predDF1 <- predict(res_photo1, newmods = c(0.5, 1.0, 1.5), addx=T)
-predDF2 <- predict(res_photo2, newmods = c(0.5, 1.0, 1.5), addx=T)
+predDF1 <- predict(res_photo1, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
+predDF2 <- predict(res_photo2, newmods = c(0.5, 1.0, 1.5, 2.0, 2.5), addx=T)
 
 predDF1$lab <- "Angiosperm"
 predDF2$lab <- "Gymnosperm"
@@ -351,15 +381,33 @@ plotDF3 <- rbind(as.data.frame(predDF1), as.data.frame(predDF2))
 
 ### make a plot
 p3 <- ggplot(plotDF3) +
-  geom_col(aes(X.VPDmean, pred, fill=lab, group=lab),
+  geom_col(aes(X.VPD_group, pred, fill=lab, group=lab),
            position=position_dodge(), col="black")+
-  geom_errorbar(aes(x=X.VPDmean, ymin=pred-se, ymax=pred+se, group=lab), 
-                position=position_dodge(), width = 0.4)
+  geom_errorbar(aes(x=X.VPD_group, ymin=pred-se, ymax=pred+se, group=lab), 
+                position=position_dodge(), width = 0.4)+
+  theme_linedraw() +
+  theme(panel.grid.minor=element_blank(),
+        axis.title.x = element_text(size=12), 
+        axis.text.x = element_text(size=12),
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_text(size=12),
+        legend.text=element_text(size=10),
+        legend.title=element_text(size=12),
+        panel.grid.major=element_blank(),
+        legend.position="right",
+        legend.text.align=0)+
+  xlab("VPD (kPa)")+
+  ylab("Photosynthesis CO2 response")
 
-plot(p3)
+#plot(p3)
 
 
+### plot
+combined_plot <- plot_grid(p1, p2, p3, 
+                           ncol=1, align="vh", axis = "l")
 
+save_plot(paste0(getwd(), "/output/predicted_response.pdf"),
+          combined_plot, base_width = 6, base_height=10)
 
 
 
